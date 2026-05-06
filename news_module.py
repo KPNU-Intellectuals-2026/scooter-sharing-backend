b5d48fced08ec7cd6c4b338ff1c5b196b08af696
Makarenko <makar@example.com>
2026-05-06 09:28:09 +0300

    Fix variable naming based on review

--- news_module.py
+++ news_module.py
@@ -1,3 +1,4 @@
-def get_news(x):
-    # TODO: fix this later
-    return ["news1", "news2"]
+def get_news(user_name):
+    # TODO: replace mock data with API request
+    news_list = ["Tech News", "Sport News", "Weather News"]
+    return news_list
